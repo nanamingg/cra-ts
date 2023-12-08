628edf969de2daceeb3ea2d9fda704f7216a40fa
nanamingg <nana172@kakao.com>
2023-12-08 15:31:09 +0900

    up

---
+++ src/1.ts
@@ -0,0 +1,13 @@
+type TProfile = { name: string; age: number; isRich: boolean };
+
+const myProfile: TProfile = {
+  name: "h662",
+  age: 18,
+  isRich: false,
+};
+
+const yourProfile: TProfile = {
+  name: "뽀삐",
+  age: 2,
+  isRich: true,
+};
